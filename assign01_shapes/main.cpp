de4fc03851cb5a72f47a6373457fab685e62371c
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-03-11 09:57:16 +0400

    add shapes in container logic (array or vector)

--- assign01_shapes/main.cpp
+++ assign01_shapes/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <vector>
 
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+struct RecShape
+{
+	sf::RectangleShape shape{};
+	sf::Vector2f velo{};
+};
 
 int main()
 {
@@ -14,10 +22,19 @@ int main()
 
 	sf::RectangleShape shape_01{ sf::Vector2f{ 100.0f, 100.0f } };
 	shape_01.setPosition(50.0f, 50.0f);
+	shape_01.setFillColor(sf::Color::Red);
+	sf::Vector2f velocity_01{ -1.0f, -2.0f };
 
-	sf::Vector2f velocity{ -1.0f, -2.0f };
+	sf::RectangleShape shape_02{ sf::Vector2f{ 100.0f, 100.0f } };
+	shape_02.setPosition(350.0f, 150.0f);
+	shape_02.setFillColor(sf::Color::Green);
+	sf::Vector2f velocity_02{ -1.0f, 2.0f };
+
+	std::array<RecShape, 2> shapes{ {{shape_01, velocity_01}, {shape_02, velocity_02}} };
+	//std::vector<RecShape> shapes{ {shape_01, velocity_01}, {shape_02, velocity_02} };
+
 	float velocity_scale{ 0.05f };
-
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -29,20 +46,22 @@ int main()
 			}
 		}
 
-		if (shape_01.getPosition().y < 0.0f || shape_01.getPosition().y + shape_01.getGlobalBounds().height > WINDOW_HEIGHT)
-		{
-			velocity.y *= -1.0f;
-		}
-		if (shape_01.getPosition().x < 0.0f || shape_01.getPosition().x + shape_01.getGlobalBounds().width > WINDOW_WIDTH)
-		{
-			velocity.x *= -1.0f;
-		}
-
-		shape_01.move(velocity * velocity_scale);
-
-		// Render
 		window.clear();
-		window.draw(shape_01);
+		
+		for (auto& shape : shapes)
+		{
+			if (shape.shape.getPosition().y < 0.0f || shape.shape.getPosition().y + shape.shape.getGlobalBounds().height > WINDOW_HEIGHT)
+			{
+				shape.velo.y *= -1.0f;
+			}
+			if (shape.shape.getPosition().x < 0.0f || shape.shape.getPosition().x + shape.shape.getGlobalBounds().width > WINDOW_WIDTH)
+			{
+				shape.velo.x *= -1.0f;
+			}
+			shape.shape.move(shape.velo * velocity_scale);
+			window.draw(shape.shape);
+		}
+
 		window.display();
 	}
 
