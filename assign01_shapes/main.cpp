75a9f59a9a2c6e2c8e46c4f58afa5f882b963c4a
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-03-17 23:57:37 +0400

    add centered text message inside the shape

--- assign01_shapes/main.cpp
+++ assign01_shapes/main.cpp
@@ -7,11 +7,8 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-struct RecShape
-{
-	sf::RectangleShape shape{};
-	sf::Vector2f velo{};
-};
+
+
 
 int main()
 {
@@ -20,19 +17,33 @@ int main()
 	const std::string WINDOW_NAME{ "Assignment01_Shapes" };
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME);
 
-	sf::RectangleShape shape_01{ sf::Vector2f{ 100.0f, 100.0f } };
-	shape_01.setPosition(50.0f, 50.0f);
-	shape_01.setFillColor(sf::Color::Red);
+	sf::Font font;
+	if (!font.loadFromFile("media/font/helvetica.ttf"))
+	{
+		std::cerr << "[ERROR]: Can't load the font, check path to the .ttf file!\n";
+		return -1;
+	}
+	sf::Text text;
+	text.setFont(font);
+	text.setCharacterSize(18);
+	text.setFillColor(sf::Color(255, 255, 255));
+	text.setString("RRed");
+
+	sf::RectangleShape rectangle_01{ sf::Vector2f{ 100.0f, 100.0f } };
+	rectangle_01.setPosition(0.0f, 0.0f);
+	rectangle_01.setFillColor(sf::Color::Red);
 	sf::Vector2f velocity_01{ -1.0f, -2.0f };
 
-	sf::RectangleShape shape_02{ sf::Vector2f{ 100.0f, 100.0f } };
-	shape_02.setPosition(350.0f, 150.0f);
-	shape_02.setFillColor(sf::Color::Green);
-	sf::Vector2f velocity_02{ -1.0f, 2.0f };
 
-	std::array<RecShape, 2> shapes{ {{shape_01, velocity_01}, {shape_02, velocity_02}} };
-	//std::vector<RecShape> shapes{ {shape_01, velocity_01}, {shape_02, velocity_02} };
+	//sf::CircleShape circle_01{ 5.0f, 30 };
+	//circle_01.setPosition(28.0f, 37.0f);
+	//circle_01.setFillColor(sf::Color::Blue);
+	//sf::Vector2f velocity_02{ 2.0f, -1.0f };
 
+	
+	//text.setPosition(circle_01.getPosition().x + (circle_01.getGlobalBounds().width - text.getGlobalBounds().width) * 0.5f, 
+	//	circle_01.getPosition().y + (circle_01.getGlobalBounds().height * 0.5f - text.getGlobalBounds().height));
+	
 	float velocity_scale{ 0.05f };
 	
 	while (window.isOpen())
@@ -47,20 +58,37 @@ int main()
 		}
 
 		window.clear();
-		
-		for (auto& shape : shapes)
+
+		//for (auto& shape : shapes)
+		//{
+		//	if (shape.shape.getPosition().x < 0.0f || shape.shape.getPosition().x + shape.shape.getGlobalBounds().width > WINDOW_WIDTH)
+		//	{
+		//		shape.velo.x *= -1.0f;
+		//	}
+		//	if (shape.shape.getPosition().y < 0.0f || shape.shape.getPosition().y + shape.shape.getGlobalBounds().height > WINDOW_HEIGHT)
+		//	{
+		//		shape.velo.y *= -1.0f;
+		//	}
+		//	shape.shape.move(shape.velo * velocity_scale);
+		//	window.draw(shape.shape);
+		//}
+
+		if (rectangle_01.getPosition().x < 0.0f || rectangle_01.getPosition().x + rectangle_01.getGlobalBounds().width > WINDOW_WIDTH)
 		{
-			if (shape.shape.getPosition().y < 0.0f || shape.shape.getPosition().y + shape.shape.getGlobalBounds().height > WINDOW_HEIGHT)
-			{
-				shape.velo.y *= -1.0f;
-			}
-			if (shape.shape.getPosition().x < 0.0f || shape.shape.getPosition().x + shape.shape.getGlobalBounds().width > WINDOW_WIDTH)
-			{
-				shape.velo.x *= -1.0f;
-			}
-			shape.shape.move(shape.velo * velocity_scale);
-			window.draw(shape.shape);
+			velocity_01.x *= -1.0f;
 		}
+		if (rectangle_01.getPosition().y < 0.0f || rectangle_01.getPosition().y + rectangle_01.getGlobalBounds().height > WINDOW_HEIGHT)
+		{
+			velocity_01.y *= -1.0f;
+		}
+
+		rectangle_01.move(velocity_01 * velocity_scale);
+
+		text.setPosition(rectangle_01.getPosition().x + (rectangle_01.getGlobalBounds().width - text.getGlobalBounds().width) * 0.5f,
+			rectangle_01.getPosition().y + (rectangle_01.getGlobalBounds().height * 0.5f - text.getGlobalBounds().height));
+
+		window.draw(rectangle_01);
+		window.draw(text);
 
 		window.display();
 	}
