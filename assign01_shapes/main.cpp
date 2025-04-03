41964a1360c291bcb08451428e8eeebb52c2a659
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-04-03 17:38:54 +0400

    add parsing txt

--- assign01_shapes/main.cpp
+++ assign01_shapes/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include <SFML/System.hpp>
@@ -6,45 +7,126 @@
 #include <SFML/Graphics.hpp>
 
 static sf::Vector2f getTextCenter(const sf::Text& text);
-
 void checkCollisionWindowBoarder(const sf::RenderWindow& window, const sf::Shape& shape, sf::Vector2f& movement_vector);
+
+struct Circle
+{
+	sf::Text name{};
+	sf::CircleShape shape{};
+	sf::Vector2f velocity{};
+	float scale{150.0f};
+};
+//
+//struct Rectangle
+//{
+//	sf::Text name{};
+//	sf::RectangleShape shape{};
+//};
+
 
 
 int main()
 {
-	const unsigned int WINDOW_WIDTH{ 800 };
-	const unsigned int WINDOW_HEIGHT{ 600 };
-	const std::string WINDOW_NAME{ "Assignment 01: Shapes" };
-	const std::string ADDRESS_FONT_FILE{ "media/font/helvetica.ttf" };
-
-
-	sf::RenderWindow window{ sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME };
-	window.setFramerateLimit(60);
-	sf::Clock clock;
-	float dt{};
-	float velocity{ 300.0f };
+	const std::string CONFIG_FILE{ "media/config/config_shapes.txt" };
 	
-	sf::Font font;
-	if (!font.loadFromFile(ADDRESS_FONT_FILE))
+	std::ifstream fin(CONFIG_FILE);
+	if (!fin)
 	{
-		std::cerr << "Can't Load Font File From: " << ADDRESS_FONT_FILE << "\n";
+		std::cerr << "Can't Open File at: " << CONFIG_FILE << "\n";
 		return -1;
 	}
-	sf::Text text_01;
-	text_01.setFont(font);
-	text_01.setCharacterSize(36);
-	text_01.setFillColor(sf::Color::Black);
-	text_01.setString("Feed");
-	text_01.setOrigin(getTextCenter(text_01));
+
+	// Window and Color
+	unsigned int window_width{};
+	unsigned int window_height{};
+	std::string window_name{};
 	
-	sf::RectangleShape rect_01{ {100.0f, 100.0f} };
-	rect_01.setFillColor(sf::Color(219, 252, 208));
-	rect_01.setPosition(400, 300);
-	sf::Vector2f move_vector{ -0.70f, 0.5f };
+	// Color
+	unsigned int color_r{};
+	unsigned int color_g{};
+	unsigned int color_b{};
+
+	// Font
+	sf::Font font;
+	std::string font_file{};
+	unsigned int font_size{};
+	sf::Color font_color{};
+	
+	// Shapes
+	Circle circle;
+	std::string circle_name{};
+	sf::Vector2f circle_position{};
+	sf::Vector2f circle_vector{};
+	float circle_radius{};
+	std::vector<Circle> circles;
+	
+	// Parse file
+	std::string line{};
+	while (fin >> line)
+	{
+		switch (line[0])
+		{
+		case('W'):
+			fin >> window_width;
+			fin >> window_height;
+
+			fin >> window_name;
+			break;
+		case('F'):
+			fin >> font_file;
+			if (!font.loadFromFile(font_file))
+			{
+				std::cerr << "Can't Load Font File at: " << font_file << "\n";
+				return -1;
+			}
+			fin >> font_size;
+			
+			fin >> color_r;
+			fin >> color_g;
+			fin >> color_b;
+			font_color = { static_cast<sf::Uint8>(color_r), static_cast<sf::Uint8>(color_g), static_cast<sf::Uint8>(color_b) };
+			break;
+		case('C'):
+			fin >> circle_name;
+			circle.name.setString(circle_name);
+			circle.name.setFont(font);
+			circle.name.setCharacterSize(font_size);
+			circle.name.setOrigin(getTextCenter(circle.name));
+
+			fin >> circle_position.x;
+			fin >> circle_position.y;
+			circle.shape.setPosition(circle_position.x, circle_position.y);
+			
+			fin >> circle_vector.x;
+			fin >> circle_vector.y;
+			circle.velocity = { circle_vector.x, circle_vector.y };
+
+			fin >> color_r;
+			fin >> color_g;
+			fin >> color_b;
+			circle.shape.setFillColor({ static_cast<sf::Uint8>(color_r), static_cast<sf::Uint8>(color_g), static_cast<sf::Uint8>(color_b) });
+			
+			fin >> circle_radius;
+			circle.shape.setRadius(circle_radius);
+			std::cout << circle_radius << '*';
+
+			circles.push_back(circle);
+
+			break;
+		case('R'):
+
+		default:
+			break;
+		}
+	}
+	sf::RenderWindow window{ sf::VideoMode(window_width, window_height), window_name };
+	
+	sf::Clock clock;
+	float dt{};
 
 	while (window.isOpen()) {
-		//auto dt{ clock.restart().asSeconds() };
 		sf::Event event;
+		dt = clock.restart().asSeconds();
 
 		while (window.pollEvent(event)) 
 		{
@@ -53,27 +135,18 @@ int main()
 				window.close();
 			}
 		}
-		dt = clock.restart().asSeconds();
-		//if (rect_01.getPosition().x < 0 || (rect_01.getPosition().x + rect_01.getGlobalBounds().width) > window.getView().getSize().x)
-		//{
-		//	move_vector.x *= -1;
-		//}
-		//if (rect_01.getPosition().y < 0 || (rect_01.getPosition().y + rect_01.getGlobalBounds().height) > window.getView().getSize().y)
-		//{
-		//	move_vector.y *= -1;
-		//}
-		checkCollisionWindowBoarder(window, rect_01, move_vector);
-
-		rect_01.move(move_vector * dt * velocity);
-		text_01.setPosition(rect_01.getPosition() + rect_01.getGlobalBounds().getSize() * 0.5f);
-
-
 		window.clear(sf::Color(194, 178, 171));
-		window.draw(rect_01);
-		window.draw(text_01);
+		for (auto& c : circles)
+		{
+			c.shape.move(c.velocity * dt * c.scale);
+			c.name.setPosition(c.shape.getPosition() + c.shape.getGlobalBounds().getSize() * 0.5f);
+			checkCollisionWindowBoarder(window, c.shape, c.velocity);
+			
+			window.draw(c.shape);
+			window.draw(c.name);
+		}
 
 		window.display();
-		//std::cout << "dt:: " << dt << '\n';
 	}
 
 	return 0;
